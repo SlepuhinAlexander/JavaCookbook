612eca67b46436254792bb0b982aa57346f8d51d
Superuser <Superuser@gmail.com>
2020-02-18 18:09:29 +0300

    Updated Chapter 2 Listing 2: made comparator locale-independent.

--- src/javacookbook/chapter2/listings/l2/IgnoreCaseComparator.java
+++ src/javacookbook/chapter2/listings/l2/IgnoreCaseComparator.java
@@ -1,10 +1,18 @@
 package javacookbook.chapter2.listings.l2;
 
+import java.text.Collator;
 import java.util.Comparator;
 
 public class IgnoreCaseComparator implements Comparator<String> {
+    private Collator collator;
+
+    public IgnoreCaseComparator() {
+        collator = Collator.getInstance();
+        collator.setStrength(Collator.PRIMARY);
+    }
+
     @Override
     public int compare(String stringA, String stringB) {
-        return stringA.compareToIgnoreCase(stringB);
+        return collator.compare(stringA, stringB);
     }
 }
