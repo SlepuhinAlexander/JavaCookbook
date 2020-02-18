ab64b6298ee7b5b548d19e380942a005b9ff6c4d
Superuser <Superuser@gmail.com>
2020-02-18 22:35:19 +0300

    Updated Chapter 2 Listing 6: made pattern case insensitive

--- src/javacookbook/chapter2/listings/l6/UseRegularExpressions.java
+++ src/javacookbook/chapter2/listings/l6/UseRegularExpressions.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 
 public class UseRegularExpressions {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("\\b\\w+@XYZ\\.com\\b");
+        Pattern p = Pattern.compile("\\b\\w+@XYZ\\.com\\b", Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher("Company\tContact\tInfo\n" +
                 "Tom\t555-1111\ttom@XYZ.com\n" +
                 "Mary\t555-2222\tMary@XYZ.com\n" +
-                "Don\t555-3333\tDon@XYZ.com");
+                "Don\t555-3333\tDon@xyz.com");
         while (m.find()) {
             System.out.println("Got a match: " + m.group());
         }
