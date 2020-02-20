dab7f1a65c7514ce2aa3bdf6f805f509ee42c889
Superuser <Superuser@gmail.com>
2020-02-20 12:15:56 +0300

    Updated Chapter 3 Listing 1: added reading buffer and relative path to file

--- src/javacookbook/chapter3/listings/l1/HexDump.java
+++ src/javacookbook/chapter3/listings/l1/HexDump.java
@@ -1,5 +1,6 @@
 package javacookbook.chapter3.listings.l1;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,26 +14,24 @@ public class HexDump {
             return;
         }
         try {
-            fis = new FileInputStream(args[0]);
+            String path = System.getProperty("java.class.path") + "/";
+            path += HexDump.class.getPackageName().replace(".","/") + "/";
+            fis = new FileInputStream(new File(path + args[0]));
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             return;
         }
         try {
             System.out.println(args[0] + " in hex:"); // for example, using the README.md
-            int i;
-            int count = 0;
+            int length;
+            byte[] data = new byte[16];
             do {
-                i = fis.read();
-                if (i != -1) {
-                    System.out.printf("%02X ", i);
-                    count++;
-                    if (count == 16) {
-                        System.out.println();
-                        count = 0;
-                    }
+                length = fis.read(data);
+                for (int i = 0; i < length; i++) {
+                    System.out.printf("%02X ", data[i]);
                 }
-            } while (i != -1);
+                System.out.println();
+            } while (length != -1);
         } catch (IOException e) {
             System.out.println("Error while reading file");
             return;
@@ -47,10 +46,6 @@ public class HexDump {
 /*
  * output:
  * README.md in hex:
- * 23 20 4A 61 76 61 20 50 72 6F 67 72 61 6D 6D 69
- * 6E 67 20 43 6F 6F 6B 62 6F 6F 6B 0D 0A 4C 69 73
- * 74 69 6E 67 73 20 61 6E 64 20 65 78 61 6D 70 6C
- * 65 73 20 66 72 6F 6D 20 48 65 72 62 20 53 63 68
- * 69 6C 64 74 27 73 20 4A 61 76 61 20 50 72 6F 67
- * 72 61 6D 6D 69 6E 67 20 43 6F 6F 6B 62 6F 6F 6B
+ * 23 23 23 20 52 65 61 64 69 6E 67 20 62 79 74 65
+ * 73 20 66 72 6F 6D 20 61 20 66 69 6C 65
  * */
