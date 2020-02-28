6c47b0c2479dd7fbf1e5415803506d32b3ab4017
Superuser <Superuser@gmail.com>
2020-02-28 13:33:12 +0300

    Added Chapter 5 Listing 7: Iterating through collections

---
+++ src/javacookbook/chapter5/listings/l7/PhoneEntry.java
@@ -0,0 +1,11 @@
+package javacookbook.chapter5.listings.l7;
+
+public class PhoneEntry {
+    String name;
+    String number;
+
+    public PhoneEntry(String n, String num) {
+        name = n;
+        number = num;
+    }
+}
